b878cc9541dab39cedd2cf8e1817ae050a3b116c
rcanoesbri <rcanoesbri@gmail.com>
2022-02-19 19:30:28 +0100

    version 1 vigenere

---
+++ src/vigenere.js
@@ -0,0 +1,37 @@
+function getLetter(num){
+    if (num>26){
+        num = num-26;
+    }
+    return String.fromCharCode(num + 65);
+}
+
+function encriptar(){
+    console.log('funciona');
+    let cipher = document.getElementById('cipher').value;
+    let lengthcipher=cipher.length;
+    cipher = cipher.toUpperCase();
+    console.log(lengthcipher);
+
+    let plaintext = document.getElementById('plaintext').value;
+    let lengthplaintext=plaintext.length;
+    plaintext = plaintext.toUpperCase();
+    let ciphernum = [];
+    let ciphertext = [];
+
+    for (let i = 0; i < lengthplaintext; i++) {
+        if (plaintext.charCodeAt(i) >= 65 && plaintext.charCodeAt(i) <= 90){
+            ciphernum[i] = plaintext.charCodeAt(i) + cipher.charCodeAt(i % lengthcipher) -65 -65;
+            ciphertext[i] = getLetter(ciphernum[i]);
+        } else {
+        ciphertext[i] = ' ';
+        }
+    }
+    console.log(ciphernum)
+    console.log(ciphertext);
+    document.getElementById('textoencriptado').innerText = ciphertext.join('');
+}
+
+window.onload = function() {
+    const boton = document.getElementById('boton');
+    boton.addEventListener('click', encriptar);
+}
