b72c21d8d56d0f6652b4ccf08b609a9d9822a02a
Raúl Cano Esbrí <5978815005@edu.gencat.cat>
2022-09-25 17:30:55 +0200

    asimetrica numeros de fins a 8 xifres

--- Asimetric/prova-asimetric/asimetricanum.js
+++ Asimetric/prova-asimetric/asimetricanum.js
@@ -10,81 +10,33 @@ let cleartext = [];
 let textfinal = [];
 let ciphertext = [];
 
-function getLetter(str){
-  let text = str;
-  let length = text.length;
-  for (i=0; i<length; i++){
-    let number = Number(text[i]);
-    textfinal[i] = String.fromCharCode(number);
-  }
-  cleartext = textfinal;
-}
-
-function getNum(str){
-  let text = str
-  let length = text.length;
-  for (i=0; i<length; i++){
-    cleartext[i] = BigInt(text.charCodeAt(i));
-  }
-  console.log(cleartext);
-}
-
-function sanitizecipher(str){
-  const from = 'ÁÀÄÂÉÈËÊÍÌÏÎÓÒÖÔÚÙÜÛÑÇ';
-  const to = 'AAAAEEEEIIIIOOOOUUUUNC';
-  for (let i=0, l=from.length; i<l; i++) {
-    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-  }
-  str.replace(/[^a-z]/g, '');
-  return str;
-}
-
-function sanitizetext(str){
-const from = 'ÁÀÄÂÉÈËÊÍÌÏÎÓÒÖÔÚÙÜÛÑÇ';
-const to = 'AAAAEEEEIIIIOOOOUUUUNC';
-for (let i=0, l=from.length; i<l; i++) {
-  str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-}
-str.replace(/[^a-z0-9]/g, '');
-return str;
-}  
-
-function processtext(){
-  let text = document.getElementById('textaprocessar').value;
-  text = text.toUpperCase();
-  text = sanitizetext(text);
-  console.log(text);
-  texto = getNum(text);
-  document.getElementById('textprocessat').innerText = cleartext;
-  console.log(cleartext);
-}
-
 function isPrime(num) {
     if (num <= 3) {
       return num > 1;
     }
-  
+
     if ((num % 2 === 0) || (num % 3 === 0)) {
       return false;
     }
-  
+
     let count = 5;
-  
+
     while (Math.pow(count, 2) <= num) {
       if (num % count === 0 || num % (count + 2) === 0) {
         return false;
       }
-  
+
       count += 6;
     }
-  
+
     return true;
-}
-  
+
+  }
+
 function randomnumber(){
   let prime = false;
   while (prime === false){
-    let num = Math.floor(Math.random() * 1000);
+    let num = Math.floor(Math.random() * 20000 + 1500);
     prime = isPrime(num);
     if (prime === true){
       console.log('works');
@@ -95,18 +47,19 @@ function randomnumber(){
 }
 
 function factorizar(num) {
+
     let factors = [];
     if (num % 2 === 0){
       factors[1] = 2;
     }
-  
+
     if (num % 3 === 0) {
       factors[2] = 3;
     }
-  
+
     let count = 5;
     let strposition = 3;
-  
+
     while (count <= num) {
       if (num % count === 0) {
         if (isPrime(count) === true){
@@ -122,7 +75,7 @@ function factorizar(num) {
       }
       count += 6;
     }
-  
+
     console.log(factors);
     return factors;
 }
@@ -149,6 +102,7 @@ function findCoprime(num) {
     }
     return coPrime;
 }
+
 
 function findj(num1, num2) {
     let found = false;
@@ -195,45 +149,29 @@ function calcularclaus(){
 }
 
 function encriptar(){
-  let length = cleartext.length;
-  for (i=0; i<length; i++){
     console.log(claus);
-    ciphertext[i] = BigInt(((cleartext[i]**claus[4]) % claus[2]));
+    cleartext = BigInt(document.getElementById('cleartext').value);
+    ciphertext = BigInt(((cleartext**claus[4]) % claus[2]));
     console.log(ciphertext);
     document.getElementById('resultat').innerText = ciphertext;
-  }
-  console.log(ciphertext);
-  let textencriptat = ciphertext.join('');
-  document.getElementById('resultat').innerText = textencriptat;
-  return ciphertext;
 } 
 
 function desencriptar(){
-  let length = ciphertext.length;
-  for (i=0; i<length; i++){
+    console.log('botonfunciona');
     console.log(claus);
-    cleartext[i] = BigInt(((ciphertext[i]**claus[5]) % claus[2]));
-    console.log(cleartext);
-    document.getElementById('resultat').innerText = cleartext;
-  }
-  console.log(cleartext);
-  let result = getLetter(cleartext);
-  let textfinal = cleartext.join('');
-  document.getElementById('resultat').innerText = textfinal;
-}
-
-function funciona (){
-  console.log('funciona');
+    console.log(ciphertext);
+    cleartext = BigInt((ciphertext ** claus[5]) % claus[2]);
+    let resultat = Number(cleartext); 
+    console.log(resultat);
+    console.log('resultat');
+    document.getElementById('resultat').innerText = resultat;
 }
 
 window.onload = function(){
     const boton1 = document.getElementById('crearclaus');
     boton1.addEventListener('click', calcularclaus);   
-    boton1.addEventListener('click', funciona);
     const boton2 = document.getElementById('encriptar');
     boton2.addEventListener('click', encriptar);
     const boton3 = document.getElementById('desencriptar');
     boton3.addEventListener('click', desencriptar);
-    const boton4 = document.getElementById('boton');
-    boton4.addEventListener('click', processtext); 
 }
