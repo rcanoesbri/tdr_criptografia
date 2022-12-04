6e61549ffd8c958a98d20a8475fae1e0a9adc175
KnoVz <hknovz@gmail.com>
2022-12-04 11:41:02 +0100

    prova socket io

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let express = require('express');
 let app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 app.get('/', function (req, res) {
   res.send('Hello World!!');
@@ -11,9 +15,22 @@ app.use(function (req, res, next) {
   res.status(404).send('Sorry, can\'t find that!');
 });
 
-let server = app.listen(3000, function () {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
   let host = server.address().address;
   let port = server.address().port;
 
   console.log('Example app listeing at http://%s:%s', host, port);
 });
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
